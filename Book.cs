d9fc19345d6c3f23e4250903b5f61b960f5b1007
FazylovV <vladislav.fazylov04@gmail.com>
2023-01-24 20:39:19 +0500

    csv parse

---
+++ Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dummy_DB
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Author { get; }
+        public string Title { get; }
+        public DateTime BorrowTime { get; set; }
+        public int Year { get; }
+        public int BookcaseNumber { get; }
+        public int ShelfNumber { get; }
+        public Reader Reader { get; set; }
+
+        public Book(int id, string author, string title, DateTime borrowTime, int year, int bookcaseNumber, int shelfNumber, Reader reader)
+        {
+            Id = id;
+            Author = author;
+            Title = title;
+            BorrowTime = borrowTime;
+            Year = year;
+            BookcaseNumber = bookcaseNumber;
+            ShelfNumber = shelfNumber;
+            Reader = reader;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Dummy_DB
+﻿using System.Text.Json;
+using System.Globalization;
+
+namespace Dummy_DB
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] readersData = File.ReadAllLines("readers.csv");
+            string[] booksData = File.ReadAllLines("books.csv");
+            List<Reader> readers = new List<Reader>();
+            List<Book> books = new List<Book>();
+            foreach (string readerData in readersData) readers.Add(CsvParser.ParseReader(readerData));
+            foreach (string bookData in booksData)
+            {
+                Book book = CsvParser.ParseBook(bookData, readers);
+                books.Add(book);
+                Console.Write($"{book.Id}|{book.Author}|{book.Title} | ");
+                if (book.Reader != null) Console.Write($"{book.Reader.Name}|{book.BorrowTime.Year}-{book.BorrowTime.Month}-{book.BorrowTime.Day}|");
+                Console.WriteLine();
+            }
         }
     }
 }
